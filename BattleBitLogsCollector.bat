bf058e5b73daeafec0dcde478290b381057aff34
livingflore <63370734+livingflore@users.noreply.github.com>
2025-06-01 16:08:56 +0500

    max archive size

--- BattleBitLogsCollector.bat
+++ BattleBitLogsCollector.bat
@@ -1,5 +1,4 @@
-
-@echo off & setlocal
+@echo off & setlocal EnableDelayedExpansion
 if _%1_==_payload_  goto :payload
 :getadmin
     echo %~nx0: elevating self
@@ -12,7 +11,7 @@ goto :eof
 :payload
 title BattleBit Logs Collector >nul
 :: i hate onedrive
-SET Logs=C:\Users\Public\Desktop\BattleBit Logs
+set Logs=C:\Users\Public\Desktop\BattleBit Logs
 if not exist "%Logs%" ( if not exist "%Logs%.zip" goto :start )
 echo [91m[X][0m BattleBit Logs folder or BattleBit Logs.zip already exists on your desktop. 
 echo Please send it to BattleBit representative in order to get support.
@@ -30,20 +29,53 @@ echo [94m[...][0m Copying EAC logs
 copy /y "%appdata%\EasyAntiCheat\anticheatlauncher.log" "%Logs%\EAC" >nul
 copy /y "%appdata%\EasyAntiCheat\service.log" "%Logs%\EAC" >nul
 copy /y "%appdata%\EasyAntiCheat\43ed9a4620fa486994c0b368cce73b5d\315826d981f4480aa6155e32d71b0d3b\loader.log" "%Logs%\EAC" >nul
+copy /y "%temp%\BattleBitEACFix.log" "%Logs%\EAC" >nul
 echo [94m[...][0m Creating msinfo32 dump
 msinfo32 /report "%Logs%\msinfo32.txt" >nul
 echo [94m[...][0m Creating dxdiag dump
 dxdiag /dontskip /t "%Logs%\dxdiag.txt" >nul
 echo [94m[...][0m Copying crash dumps
+
+set BigDumpCollected=0
+:: Max size considering compressing (0.7)
+set MaxDumpSize=46215620 
+set MaxTotalSize=209715200
+set TotalDumpSize=0
+set SkippedDumps=""
+
 for /R "C:\Users\%username%\AppData\Local\CrashDumps" %%F in (*BattleBit*.dmp *EasyAntiCheat*.dmp) do (
-  copy /y %%F "%Logs%\Dumps" >nul
+  set FileSize="%%~zF"
+  set /a "NewTotalSize=!TotalDumpSize!+!FileSize!"
+  
+  if !NewTotalSize! GTR !MaxTotalSize! (
+    set /a "SkippedDumps+=%%~nxF "
+  ) else (
+    if %BigDumpCollected%==0 (
+      if !FileSize! GTR !MaxDumpSize! (
+        copy /y "%%F" "%Logs%\Dumps" >nul
+        set BigDumpCollected=1
+        set /a "TotalDumpSize+=!FileSize!"
+      ) else (
+        copy /y "%%F" "%Logs%\Dumps" >nul
+        set /a "TotalDumpSize+=!FileSize!"
+      )
+    ) else (
+      if !FileSize! LEQ !MaxDumpSize! (
+        copy /y "%%F" "%Logs%\Dumps" >nul
+        set /a "TotalDumpSize+=!FileSize!"
+      )
+    )
+  )
 )
+if not %SkippedDumps%=="" ( echo %SkippedDumps% > "%Logs%\Dumps\SkippedDumps.txt" )
+
 echo [94m[...][0m Creating BattleBit Logs.zip
 powershell Compress-Archive '%Logs%' '%Logs%.zip' >nul
 echo.
 echo [32m[+][0m Finished!
 echo.
-echo [93mPlease copy BattleBit Logs folder from your desktop 
-echo and send it to BattleBit representative in order to get support.[0m
+echo [93mPlease copy BattleBit Logs.zip file from your desktop 
+echo and send it to BattleBit representative in order to get support.
+echo If you don't see the archive on your desktop, hit F5.[0m
 echo.
 pause
